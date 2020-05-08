c64627458bc28f1f9780cf92d31e9cfec6a7ed3a
Pete Lewis <pete.lewis@sparkfun.local>
2020-05-08 13:19:37 -0600

    ex1

---
+++ examples/ex1_qwiic_max3010x.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+#-----------------------------------------------------------------------------
+# ex1_qwiic_MAX3010x.py
+#
+# Simple example for the qwiic MAX3010x device
+#------------------------------------------------------------------------
+#
+# Written by Pete Lewis
+# SparkFun Electronics, May 2020
+# 
+# This python library supports the SparkFun Electroncis qwiic 
+# qwiic sensor/board ecosystem on a Raspberry Pi (and compatable) single
+# board computers. 
+#
+# More information on qwiic is at https://www.sparkfun.com/qwiic
+#
+# Do you like this library? Help support SparkFun. Buy a board!
+#
+#==================================================================================
+# Copyright (c) 2019 SparkFun Electronics
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy 
+# of this software and associated documentation files (the "Software"), to deal 
+# in the Software without restriction, including without limitation the rights 
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+# copies of the Software, and to permit persons to whom the Software is 
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all 
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+# SOFTWARE.
+#==================================================================================
+# Example 1
+#
+
+from __future__ import print_function
+import qwiic_max3010x
+import time
+import sys
+
+def runExample():
+
+	print("\nSparkFun MAX3010x Particle Sensor - Example 1\n")
+	particleSensor = qwiic_max3010x.QwiicMax3010x()
+
+	if particleSensor.begin() == False:
+		print("The Qwiic MAX3010x device isn't connected to the system. Please check your connection", \
+			file=sys.stderr)
+		return
+    else:
+        print("The Qwiic MAX3010x is connected.")
+
+	if particleSensor.setup() == False:
+		print("Device setup failure. Please check your connection", \
+			file=sys.stderr)
+		return
+    else:
+        print("Setup complete.")        
+
+	while True:
+			print(\
+			 '{: 06d}'.format(particleSensor.axRaw)\
+			, '\t', 'R[', particleSensor.getRed() , ']'\
+            , '\t', 'IR[', particleSensor.getIR() , ']'\
+            , '\t', 'G[', particleSensor.getGreen() , ']'
+			)
+			time.sleep(0.03)
+		else:
+			print("Waiting for data")
+			time.sleep(0.5)
+
+if __name__ == '__main__':
+	try:
+		runExample()
+	except (KeyboardInterrupt, SystemExit) as exErr:
+		print("\nEnding Example 1")
+		sys.exit(0)
+
+
