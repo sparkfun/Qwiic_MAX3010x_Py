3b9bd6b4488e40774f7092a381e3fb9d4d67b758
Dryw Wade <dryw.wade@sparkfun.com>
2023-12-22 17:06:20 -0700

    Check provided address and ping on correct bus

--- qwiic_max3010x/qwiic_max3010x.py
+++ qwiic_max3010x/qwiic_max3010x.py
@@ -251,7 +251,10 @@ class QwiicMax3010x(object):
     def __init__(self, address=None, i2c_driver=None):
 
         # Did the user specify an I2C address?
-        self.address = address if address is not None else self.available_addresses[0]
+        if address in self.available_addresses:
+            self.address = address
+        else:
+            self.address = self.available_addresses[0]
 
         # load the I2C driver if one isn't provided
 
@@ -276,7 +279,7 @@ class QwiicMax3010x(object):
             :rtype: bool
 
         """
-        return qwiic_i2c.isDeviceConnected(self.address)
+        return self._i2c.isDeviceConnected(self.address)
 
     connected = property(is_connected)
 
